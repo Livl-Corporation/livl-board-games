8e79b8b6e626e24e4a6ec01128e083d440c67a39
Franck GUTMANN <franck.gutmann@pixacare.com>
2022-10-23 13:15:32 +0200

    implement triangle classes

---
+++ src/TP2/functions.hpp
@@ -0,0 +1,4 @@
+#include "point.hpp"
+#include <cmath>
+
+float distanceBetweenPoints(Point point1, Point point2);
--- src/TP2/triangle.cpp
+++ src/TP2/triangle.cpp
@@ -1,24 +1,47 @@
 #include "triangle.hpp"
 #include <array>
+#include "functions.hpp"
 
 inline float Triangle::base() const
 {
+    std::array<float, 3> lengths = this->getLengths();
+    float base = 0.0;
+    for (int i = 0; i < 3; i++)
+    {
+        if (lengths[i] > base)
+        {
+            base = lengths[1];
+        }
+    }
+    return base;
 }
 
 inline float Triangle::height() const
 {
+    return this->surface() / (this->base() / 2);
 }
 
 inline float Triangle::surface() const
 {
+    std::array<float, 3> lengths = this->getLengths();
+    float s = (lengths[0] + lengths[1] + lengths[2]) / 2.0;
+    return std::sqrt(s * (s - lengths[0]) * (s - lengths[1]) * (s - lengths[2]));
 }
 
 inline std::array<float, 3> Triangle::getLengths() const
 {
+    float distance1 = distanceBetweenPoints(this->getPoint1(), this->getPoint2());
+    float distance2 = distanceBetweenPoints(this->getPoint2(), this->getPoint3());
+    float distance3 = distanceBetweenPoints(this->getPoint3(), this->getPoint1());
+    return {distance1, distance2, distance3};
 }
 
 inline bool Triangle::isIsoceles() const
 {
+    std::array<float, 3> lengths = this->getLengths();
+    return (lengths[0] == lengths[1] ||
+            lengths[1] == lengths[2] ||
+            lengths[2] == lengths[0]);
 }
 
 inline bool Triangle::isRectangle() const
@@ -27,4 +50,6 @@ inline bool Triangle::isRectangle() const
 
 inline bool Triangle::isEquilateral() const
 {
+    std::array<float, 3> lengths = this->getLengths();
+    return (lengths[0] == lengths[1] && lengths[1] == lengths[2]);
 }
