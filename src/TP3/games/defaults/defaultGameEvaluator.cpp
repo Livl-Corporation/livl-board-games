1e3a8be9032ab26e4f3873adb43bac45c55aafc6
Franck GUTMANN <franck.gutmann@pixacare.com>
2022-12-08 15:05:22 +0100

    refactor default game evaluator

--- src/TP3/games/defaults/defaultGameEvaluator.cpp
+++ src/TP3/games/defaults/defaultGameEvaluator.cpp
@@ -4,47 +4,68 @@ bool DefaultGameEvaluator::hasPlayerWon(const unsigned int id, const Grid &grid)
 {
     unsigned int maxConsecutive = 0;
 
-    auto checkMaxConsecutive = [&](unsigned int startRow, unsigned int startCol, unsigned int rowStep, unsigned int colStep)
-    {
-        unsigned int curMaxConsecutive = 0;
-        for (unsigned int row = startRow, col = startCol; row < grid.getYSize() && col < grid.getXSize(); row += rowStep, col += colStep)
-        {
-            if (grid.getCell({x : col, y : row}) == id)
-            {
-                curMaxConsecutive++;
-            }
-            else
-            {
-                curMaxConsecutive = 0;
-            }
-        }
-        if (curMaxConsecutive > maxConsecutive)
-        {
-            maxConsecutive = curMaxConsecutive;
-        }
-    };
-
     // Lignes
-    for (unsigned int row = 0; row < grid.getYSize(); row++)
-    {
-        checkMaxConsecutive(row, 0, 01, 1);
-    }
+    checkRows(id, grid, maxConsecutive);
+    if (maxConsecutive >= this->consecutiveIdsToWin)
+        return true;
 
     // Colonnes
-    for (unsigned int col = 0; col < grid.getXSize(); col++)
-    {
-        checkMaxConsecutive(0, col, 1, 0);
-    }
+    checkColumns(id, grid, maxConsecutive);
+    if (maxConsecutive >= this->consecutiveIdsToWin)
+        return true;
 
     // Diagonales
+    checkDiagonals(id, grid, maxConsecutive);
+    if (maxConsecutive >= this->consecutiveIdsToWin)
+        return true;
+
+    return false;
+}
+
+void DefaultGameEvaluator::checkRows(const unsigned int id, const Grid &grid, unsigned int &maxConsecutive)
+{
+    for (unsigned int row = 0; row < grid.getYSize(); row++)
+    {
+        checkMaxConsecutive(id, grid, row, 0, 0, 1, maxConsecutive);
+    }
+}
+
+void DefaultGameEvaluator::checkColumns(const unsigned int id, const Grid &grid, unsigned int &maxConsecutive)
+{
+    for (unsigned int col = 0; col < grid.getXSize(); col++)
+    {
+        checkMaxConsecutive(id, grid, 0, col, 1, 0, maxConsecutive);
+    }
+}
+
+void DefaultGameEvaluator::checkDiagonals(const unsigned int id, const Grid &grid, unsigned int &maxConsecutive)
+{
     unsigned int maxCol = grid.getXSize() - grid.getYSize();
     for (unsigned int startCol = 0; startCol <= maxCol; startCol++)
     {
-        checkMaxConsecutive(0, startCol, 1, 1);
-        checkMaxConsecutive(0, startCol, 1, -1);
-        checkMaxConsecutive(grid.getYSize() - 1, startCol, -1, 1);
-        checkMaxConsecutive(grid.getYSize() - 1, startCol, -1, -1);
+        checkMaxConsecutive(id, grid, 0, startCol, 1, 1, maxConsecutive);
+        checkMaxConsecutive(id, grid, 0, startCol, 1, -1, maxConsecutive);
+        checkMaxConsecutive(id, grid, grid.getYSize() - 1, startCol, -1, 1, maxConsecutive);
+        checkMaxConsecutive(id, grid, grid.getYSize() - 1, startCol, -1, -1, maxConsecutive);
     }
+}
 
-    return maxConsecutive >= this->consecutiveIdsToWin;
+void DefaultGameEvaluator::checkMaxConsecutive(const unsigned int id, const Grid &grid, unsigned int startRow, unsigned int startCol, unsigned int rowStep, unsigned int colStep, unsigned int &maxConsecutive)
+{
+    unsigned int curMaxConsecutive = 0;
+    for (unsigned int row = startRow, col = startCol; row < grid.getYSize() && col < grid.getXSize(); row += rowStep, col += colStep)
+    {
+        if (grid.getCell({x : col, y : row}) == id)
+        {
+            curMaxConsecutive++;
+        }
+        else
+        {
+            curMaxConsecutive = 0;
+        }
+    }
+    if (curMaxConsecutive > maxConsecutive)
+    {
+        maxConsecutive = curMaxConsecutive;
+    }
 }
--- src/TP3/games/defaults/defaultGameEvaluator.hpp
+++ src/TP3/games/defaults/defaultGameEvaluator.hpp
@@ -16,4 +16,13 @@ public:
      * @return false
      */
     bool hasPlayerWon(const unsigned int id, const Grid &grid) override;
+
+private:
+    void checkRows(const unsigned int id, const Grid &grid, unsigned int &maxConsecutive);
+
+    void checkColumns(const unsigned int id, const Grid &grid, unsigned int &maxConsecutive);
+
+    void checkDiagonals(const unsigned int id, const Grid &grid, unsigned int &maxConsecutive);
+
+    void checkMaxConsecutive(const unsigned int id, const Grid &grid, unsigned int startRow, unsigned int startCol, unsigned int rowStep, unsigned int colStep, unsigned int &maxConsecutive);
 };
