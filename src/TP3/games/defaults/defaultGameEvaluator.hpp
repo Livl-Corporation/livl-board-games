1b47c557dce2d1cb71762cdd30864677002c5450
Franck GUTMANN <franck.gutmann@pixacare.com>
2022-12-08 15:08:57 +0100

    refactor default game evaluator

--- src/TP3/games/defaults/defaultGameEvaluator.hpp
+++ src/TP3/games/defaults/defaultGameEvaluator.hpp
@@ -15,14 +15,14 @@ public:
      * @return true
      * @return false
      */
-    bool hasPlayerWon(const unsigned int id, const Grid &grid) override;
+    bool hasPlayerWon(const unsigned int id, const Grid &grid) const override;
 
 private:
-    void checkRows(const unsigned int id, const Grid &grid, unsigned int &maxConsecutive);
+    void checkRows(const unsigned int id, const Grid &grid, unsigned int &maxConsecutive) const;
 
-    void checkColumns(const unsigned int id, const Grid &grid, unsigned int &maxConsecutive);
+    void checkColumns(const unsigned int id, const Grid &grid, unsigned int &maxConsecutive) const;
 
-    void checkDiagonals(const unsigned int id, const Grid &grid, unsigned int &maxConsecutive);
+    void checkDiagonals(const unsigned int id, const Grid &grid, unsigned int &maxConsecutive) const;
 
-    void checkMaxConsecutive(const unsigned int id, const Grid &grid, unsigned int startRow, unsigned int startCol, unsigned int rowStep, unsigned int colStep, unsigned int &maxConsecutive);
+    void checkMaxConsecutive(const unsigned int id, const Grid &grid, unsigned int startRow, unsigned int startCol, unsigned int rowStep, unsigned int colStep, unsigned int &maxConsecutive) const;
 };
--- src/TP3/interfaces/gameEvaluator.hpp
+++ src/TP3/interfaces/gameEvaluator.hpp
@@ -17,7 +17,7 @@ public:
      * @return true
      * @return false
      */
-    virtual bool hasPlayerWon(const unsigned int id, const Grid &grid) = 0;
+    virtual bool hasPlayerWon(const unsigned int id, const Grid &grid) const = 0;
 
 protected:
     unsigned int consecutiveIdsToWin;
