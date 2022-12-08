9e61b9d65cdb6fcc318a7afeb61af1699560c584
Franck GUTMANN <franck.gutmann@pixacare.com>
2022-12-08 14:48:52 +0100

    refactor default game evaluator

--- src/TP3/games/defaults/defaultGameEvaluator.cpp
+++ src/TP3/games/defaults/defaultGameEvaluator.cpp
@@ -2,102 +2,48 @@
 
 bool DefaultGameEvaluator::hasPlayerWon(const unsigned int id, const Grid &grid)
 {
-
     unsigned int maxConsecutive = 0;
 
-    // Horizontals
+    auto checkMaxConsecutive = [&](unsigned int startRow, unsigned int startCol, unsigned int rowStep, unsigned int colStep)
+    {
+        unsigned int curMaxConsecutive = 0;
+        for (unsigned int row = startRow, col = startCol; row < grid.getYSize() && col < grid.getXSize(); row += rowStep, col += colStep)
+        {
+            if (grid.getCell({x : col, y : row}) == id)
+            {
+                curMaxConsecutive++;
+            }
+            else
+            {
+                curMaxConsecutive = 0;
+            }
+        }
+        if (curMaxConsecutive > maxConsecutive)
+        {
+            maxConsecutive = curMaxConsecutive;
+        }
+    };
+
+    // Lignes
     for (unsigned int row = 0; row < grid.getYSize(); row++)
     {
-
-        unsigned int rowMaxConsecutive = 0;
-        for (unsigned int col = 0; col < grid.getXSize(); col++)
-        {
-            if (grid.getCell({x : col, y : row}) == id)
-            {
-                rowMaxConsecutive++;
-            }
-            else
-            {
-                rowMaxConsecutive = 0;
-            }
-        }
-
-        if (rowMaxConsecutive > maxConsecutive)
-        {
-            maxConsecutive = rowMaxConsecutive;
-        }
+        checkMaxConsecutive(row, 0, 01, 1);
     }
 
-    // Verticals
+    // Colonnes
     for (unsigned int col = 0; col < grid.getXSize(); col++)
     {
-
-        unsigned int colMaxConsecutive = 0;
-        for (unsigned int row = 0; row < grid.getYSize(); row++)
-        {
-            if (grid.getCell({x : col, y : row}) == id)
-            {
-                colMaxConsecutive++;
-            }
-            else
-            {
-                colMaxConsecutive = 0;
-            }
-        }
-
-        if (colMaxConsecutive > maxConsecutive)
-        {
-            maxConsecutive = colMaxConsecutive;
-        }
+        checkMaxConsecutive(0, col, 1, 0);
     }
 
-    // Diagonals
+    // Diagonales
     unsigned int maxCol = grid.getXSize() - grid.getYSize();
     for (unsigned int startCol = 0; startCol <= maxCol; startCol++)
     {
-
-        unsigned int upMaxConsecutive = 0;
-        unsigned int downMaxConsecutive = 0;
-
-        for (unsigned int y = 0; y < grid.getYSize(); y++)
-        {
-
-            // Diagonal going down
-            if (grid.getCell({
-                    x : (startCol + y),
-                    y : y
-                }) == id)
-            {
-                downMaxConsecutive++;
-            }
-            else
-            {
-                downMaxConsecutive = 0;
-            }
-
-            // Diagonal going up
-            if (grid.getCell({
-                    x : (startCol + y),
-                    y : (grid.getYSize() - 1 - y)
-                }) == id)
-            {
-                upMaxConsecutive++;
-            }
-            else
-            {
-                upMaxConsecutive = 0;
-            }
-        }
-
-        if (upMaxConsecutive > maxConsecutive)
-        {
-            maxConsecutive = upMaxConsecutive;
-        }
-
-        if (downMaxConsecutive > maxConsecutive)
-        {
-            maxConsecutive = downMaxConsecutive;
-        }
+        checkMaxConsecutive(0, startCol, 1, 1);
+        checkMaxConsecutive(0, startCol, 1, -1);
+        checkMaxConsecutive(grid.getYSize() - 1, startCol, -1, 1);
+        checkMaxConsecutive(grid.getYSize() - 1, startCol, -1, -1);
     }
 
     return maxConsecutive >= this->consecutiveIdsToWin;
