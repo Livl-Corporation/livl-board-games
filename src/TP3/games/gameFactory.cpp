3b8de27bd6eaddf1e2a7570a7b29cc3e08238dc3
Franck GUTMANN <franck.gutmann@pixacare.com>
2022-12-18 16:30:26 +0100

    feat(othello): implement othello game & othello grid

--- src/TP3/games/gameFactory.cpp
+++ src/TP3/games/gameFactory.cpp
@@ -2,6 +2,25 @@
 
 namespace GameFactory
 {
+
+    std::unique_ptr<Game> createGame(const unsigned int gameId, const std::vector<Player> &players)
+    {
+        if (gameId == 1)
+        {
+            return GameFactory::createTicTacToe(players);
+        }
+        else if (gameId == 2)
+        {
+            return GameFactory::createPower4(players);
+        }
+        else if (gameId == 3)
+        {
+            return GameFactory::createOthello(players);
+        }
+
+        return nullptr;
+    }
+
     std::unique_ptr<Game> createPower4(const std::vector<Player> &players)
     {
         return std::make_unique<Power4>(players, std::make_unique<Power4PositionRequester>(), std::make_unique<LinearGameEvaluator>(Power4::pointsToWin));
@@ -10,5 +29,10 @@ namespace GameFactory
     std::unique_ptr<Game> createTicTacToe(const std::vector<Player> &players)
     {
         return std::make_unique<TicTacToe>(players, std::make_unique<TicTacToePositionRequester>(), std::make_unique<LinearGameEvaluator>(TicTacToe::pointsToWin));
+    };
+
+    std::unique_ptr<Game> createOthello(const std::vector<Player> &players)
+    {
+        return std::make_unique<Othello>(players, std::make_unique<TicTacToePositionRequester>(), std::make_unique<LinearGameEvaluator>(TicTacToe::pointsToWin));
     };
 }
--- src/TP3/games/gameFactory.hpp
+++ src/TP3/games/gameFactory.hpp
@@ -7,15 +7,21 @@
 
 #include "../game.hpp"
 
-#include "power4/power4.hpp"
 #include "tictactoe/tictactoe.hpp"
-
-#include "power4/power4PositionRequester.hpp"
 #include "tictactoe/tictactoePositionRequester.hpp"
+
+#include "power4/power4.hpp"
+#include "power4/power4PositionRequester.hpp"
+
+#include "othello/othello.hpp"
+
 #include "../shared/evaluators/linearGameEvaluator.hpp"
 
 namespace GameFactory
 {
+    std::unique_ptr<Game> createGame(const unsigned int gameId, const std::vector<Player> &players);
     std::unique_ptr<Game> createPower4(const std::vector<Player> &players);
     std::unique_ptr<Game> createTicTacToe(const std::vector<Player> &players);
+    std::unique_ptr<Game> createOthello(const std::vector<Player> &players);
+
 }
