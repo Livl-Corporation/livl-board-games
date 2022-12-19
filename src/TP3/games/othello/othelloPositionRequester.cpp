a3e6ec768eb2a298d62fcd50bd8df66a6f0e297d
Julien Von Der Marck <vdmjulien05@gmail.com>
2022-12-19 22:00:09 +0100

    feat(OthelloPositionRequester): implement system to place the token in a valid position

--- src/TP3/games/othello/othelloPositionRequester.cpp
+++ src/TP3/games/othello/othelloPositionRequester.cpp
@@ -2,34 +2,59 @@
 
 Position OthelloPositionRequester::askForPosition(const char playerChar, const Grid<PlayerId> &grid) const
 {
-    std::string outputAskPlayer = "Où voulez vous placer votre pion (";
-    outputAskPlayer += playerChar;
-    outputAskPlayer += ") entre 1,1 et " + std::to_string(grid.getXSize()) + "," + std::to_string(grid.getYSize()) + " ?";
-
-    ConsoleHandler::printLine(outputAskPlayer);
-
-    unsigned int x, y;
-    int scanfResult = 0;
-    do
+    while (true)
     {
-        // Read the input as a string and check for the comma separator
-        std::string input = ConsoleHandler::getInput();
-        if (input.find(',') != std::string::npos)
-        { // std::string::npos is returned if no comma is found
-            // If the comma separator is found, use sscanf to parse the x and y values
-            scanfResult = sscanf(input.c_str(), "%d,%d", &x, &y);
-        }
-        else
+        std::cout << "Player " << playerChar << ", enter the row and column where you want to place your token (e.g. 3 4): ";
+        int row, col;
+        std::cin >> row >> col;
+
+        Position pos{ static_cast<unsigned int>(col - 1), static_cast<unsigned int>(row - 1) };
+        if (grid.isPositionInBounds(pos) && grid.isPositionEmpty(pos))
         {
-            // Otherwise, reset the scanfResult to 0
-            scanfResult = 0;
+            // Check if the position is valid according to the rules of the Reversi game
+            char opponentChar = (playerChar == 'X') ? 'O' : 'X';
+            if (canPlaceToken(pos, playerChar, opponentChar, grid))
+            {
+                return pos;
+            }
         }
 
-        if (scanfResult != 2)
-        {
-            ConsoleHandler::printLine("Entrée invalide. Veuillez entrer deux nombres séparés par une virgule.");
-        }
-    } while (scanfResult != 2);
+        std::cout << "Invalid position. Please try again." << std::endl;
+    }
+}
 
-    return {(x - 1), (y - 1)};
+bool OthelloPositionRequester::canPlaceToken(const Position &pos, char playerChar, char opponentChar, const Grid<PlayerId> &grid) const
+{
+    // Check the eight possible directions from the position that the player choose to place his token
+    // (left-up, up, right-up, right, right-down, down, left-down, left)
+    static const std::vector<int> dx{ -1, -1, -1, 0, 1, 1,  1,  0 };
+    static const std::vector<int> dy{ -1,  0,  1, 1, 1, 0, -1, -1 };
+
+    for (int i = 0; i < 8; i++)
+    {
+        int x = pos.x + dx[i];
+        int y = pos.y + dy[i];
+        bool foundOpponent = false;
+        while (grid.isPositionInBounds({ static_cast<unsigned int>(x), static_cast<unsigned int>(y) }))
+        {
+            char cell = Player::getPlayerChar(grid.getElementAt({ static_cast<unsigned int>(x), static_cast<unsigned int>(y) }));
+            if (cell == opponentChar)
+            {
+                foundOpponent = true;
+            }
+            else if (cell == playerChar && foundOpponent)
+            {
+                // We found a sequence of opponent's tokens that can be flipped
+                return true;
+            }
+            else
+            {
+                // We reached a blank cell or a sequence of our own tokens, so we can stop searching in this direction
+                break;
+            }
+            x += dx[i];
+            y += dy[i];
+        }
+    }
+    return false;
 }
--- src/TP3/games/othello/othelloPositionRequester.hpp
+++ src/TP3/games/othello/othelloPositionRequester.hpp
@@ -3,5 +3,8 @@
 #include "../../interfaces/positionRequester.hpp"
 
 class OthelloPositionRequester : public PositionRequester{
+public:
     Position askForPosition(const char playerChar, const Grid<PlayerId> &grid) const override;
+private:
+    bool canPlaceToken(const Position &pos, char playerChar, char opponentChar, const Grid<PlayerId> &grid) const;
 };
