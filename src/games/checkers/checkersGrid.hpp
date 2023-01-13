1653f0cd5bd7efdd08e60775dc15c5e19be034b0
Julien Von Der Marck <vdmjulien05@gmail.com>
2023-01-13 18:08:43 +0100

    feat(GridCheckers): place default token on the board for a checkers game for element 1 and 2

--- src/games/checkers/checkersGrid.hpp
+++ src/games/checkers/checkersGrid.hpp
@@ -6,7 +6,23 @@
 class CheckersGrid : public Grid<PlayerId>
 {
 public:
-    CheckersGrid() : Grid(xSize, ySize, NO_PLAYER){};
+    CheckersGrid() : Grid(xSize, ySize, NO_PLAYER){
+        // place default token on the board for a checkers game for element 1 and 2 in horizontal direction
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < xSize; j++) {
+                if ((i + j) % 2 == 1) {
+                    this->place({j, i}, 1);
+                }
+            }
+        }
+        for (int i = ySize - 1; i > ySize - 4; i--) {
+            for (int j = 0; j < xSize; j++) {
+                if ((i + j) % 2 == 1) {
+                    this->place({j, i}, 2);
+                }
+            }
+        }
+    };
     explicit CheckersGrid(const Grid<PlayerId> &other) : Grid<PlayerId>(other) {}
 
     static constexpr int xSize = 8;
