d09eb295544c534895cbde3bb50033890d87cb61
Julien Von Der Marck <vdmjulien05@gmail.com>
2023-01-31 09:20:55 +0100

    feat(CheckersEvaluator): implement checkers evaluator

--- src/models/evaluators/CheckersEvaluator.h
+++ src/models/evaluators/CheckersEvaluator.h
@@ -15,7 +15,6 @@ public:
     bool hasGameEnded(const Grid<Token> &grid, const PlayerId &nextPlayerId) override;
 
     [[nodiscard]] PlayerId getWinner(const Grid<Token> &grid) const override;
-private:
 
     bool isMoveValid(std::shared_ptr<Grid<Token>> &grid, const PlayerId &playerId, const Position &from, const Position &to) const;
 
