dc0f9c64d876cc1e2b4f6ef2ae73d716f4e1b258
Franck GUTMANN <franck.gutmann@pixacare.com>
2023-01-31 17:10:11 +0100

    restore shared

---
+++ src/shared/Shared.h
@@ -0,0 +1,15 @@
+//
+// Created by Franck GUTMANN on 31/01/2023.
+//
+
+#ifndef LIVL_SHARED_H
+#define LIVL_SHARED_H
+
+#include <random>
+
+namespace Shared
+{
+    int randomInt(int min, int max);
+}
+
+#endif //LIVL_SHARED_H
